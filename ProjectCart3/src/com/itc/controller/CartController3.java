e25628c1070a462846abcf81b5b8776dedd872bb
Mohammed Akramulla <32752083+MAk0007@users.noreply.github.com>
2018-09-14 18:57:26 +0530

    Update CartController3.java

--- ProjectCart3/src/com/itc/controller/CartController3.java
+++ ProjectCart3/src/com/itc/controller/CartController3.java
@@ -22,7 +22,7 @@ public class CartController3 {
 	@Autowired
 	ProductDAO ProductDAO;
 	
-	   @RequestMapping(value = "/homepage", method = RequestMethod.GET)
+	   @RequestMapping(value = "/", method = RequestMethod.GET)
 	   public String redirect2() {
 	      return "redirect:/pages/homepage.html";
 	   }
